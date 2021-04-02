47976775c20273ba85a8bf17908c747a6b1c2e00
Donskoi Aleksandr <donskoy.alexander@gmail.com>
2021-04-02 09:29:18 +0300

    add config

--- csv_cutter/__main__.py
+++ csv_cutter/__main__.py
@@ -1,3 +1,4 @@
+import configparser 
 import csv
 import logging
 
@@ -5,9 +6,15 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
+config = configparser.ConfigParser()
+config.read("settings.ini") 
+DELIMITER = config["in"]["delimiter"]
+ENCODINGS=config["in"]["encodings"].split(", ")
+ENCODING_OUT=config["out"]["encoding"]
+
 
 def cut_csv(file_path):
-    for encode in ("utf-8", "latin-1", "utf-8-sig", "cp1251", "1252"):
+    for encode in ENCODINGS:
         lines = 10
         if not isinstance(file_path, str):
             file_path = file_path.decode("utf-8")
@@ -15,9 +22,9 @@ def cut_csv(file_path):
         new_path = ".".join((f"{file_path.split('.')[0]}_10_lines", "csv"))
         try:
             with open(file_path, "r", newline="",  encoding=encode) as f:
-                with open(new_path, "w") as f_write:
-                    writer = csv.writer(f_write, delimiter=";")
-                    reader = csv.reader(f, delimiter=";")
+                with open(new_path, "w", encoding=ENCODING_OUT) as f_write:
+                    writer = csv.writer(f_write, delimiter=DELIMITER)
+                    reader = csv.reader(f, delimiter=DELIMITER)
                     header = next(reader)
                     n = 0
                     writer.writerow(header)
